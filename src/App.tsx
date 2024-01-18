39870df6eb6953314257e6eb9ba6585393f82b79
Benjamin Haines <bhaines@yext.com>
2024-01-18 03:18:37 -0500

    feat: shareable links and results

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { For, createEffect, createSignal, Show } from 'solid-js'
 import Mexp from 'math-expression-evaluator';
-import { IoWarning } from 'solid-icons/io'
+import { IoWarning, IoShareSocialSharp } from 'solid-icons/io'
 import classNames from 'classnames';
+import { Toaster, toast } from 'solid-toast';
+import {
+  Route,
+  Router,
+  useSearchParams,
+} from "@solidjs/router";
 import './App.css'
+
+const WINNING_SCORE = 20;
+const TOTAL_TIME = 180;
 
 function getRandomArbitrary(min: number, max: number) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -10,14 +19,29 @@ function getRandomArbitrary(min: number, max: number) {
 
 const defaultCompleted = [...new Array(20)].reduce((out, _, i) => {out[i+1] = ""; return out}, {})
 
+function hash(n: any): number {
+  return ((0x0000FFFF & n)<<16) + ((0xFFFF0000 & n)>>16)
+}
+
 function App() {
+  return (
+    <Router>
+      <Route path="/*" component={InnerApp} />
+    </Router>
+  )
+}
+
+function InnerApp() {
   const [digits, setDigits] = createSignal<string[]>(["?", "?", "?", "?"]);
   const [inProgress, setInProgress] = createSignal(false);
   const [time, setTime] = createSignal(0);
+  const [initialParamsUsed, setInitialParamsUsed] = createSignal(false);
+  const [gameStarted, setGameStarted] = createSignal(false);
   const [output, setOutput] = createSignal(" ");
   const [completed, setCompleted] = createSignal<Record<number, {expr: string, onTime: boolean}>>(defaultCompleted)
   const [expressionIsValid, setExpressionIsValid] = createSignal(true);
   const [tooltipShown, setTooltipShown] = createSignal(false);
+  const [params, setParams] = useSearchParams();
 
   const expectedCounts = () => {
     const m: Record<string, number> = {}
@@ -27,10 +51,17 @@ function App() {
     return m
   }
 
-  setInterval(() => setTime(time => inProgress() ? time - 1 : time), 1000)
+  setInterval(() => setTime(time => inProgress() ? time - 1 : time), 1000);
+  const numbersSolved = () => Object.entries(completed()).reduce((tot, cur) => tot + (cur[1].onTime ? 1 : 0), 0);
 
   createEffect(() => {
     if (time() <= 0) {
+      setInProgress(false);
+    }
+  })
+  
+  createEffect(() => {
+    if (numbersSolved() === WINNING_SCORE) {
       setInProgress(false);
     }
   })
@@ -38,12 +69,19 @@ function App() {
   let ref: HTMLInputElement | undefined;
 
   const handle = () => {
-    const newDigits = [...new Array(4)].map(_ => String(getRandomArbitrary(1, 10)));
+    const initialGame = params['game'];
+    const newDigits = (initialParamsUsed() || !initialGame)
+      ? [...new Array(4)].map(_ => String(getRandomArbitrary(1, 10)))
+      : String(hash(initialGame)).split("");
+
+    setGameStarted(true);
     setDigits(newDigits);
+    setParams({game: hash(newDigits.join(''))})
+    setInitialParamsUsed(true);
     setCompleted(defaultCompleted);
     setExpressionIsValid(true);
     setInProgress(true);
-    setTime(180);
+    setTime(TOTAL_TIME);
   }
 
   const handle2 = () => {
@@ -84,6 +122,7 @@ function App() {
 
   return (
     <div class="container my-4 flex flex-col items-center">
+      <Toaster />
       <h1 class="mb-4">{inProgress() ? <>{Math.floor(time() / 60)}: {String(time() % 60).padStart(2, "0")}</>: <>Number Boggle</>}</h1>
       <button class="w-full max-w-[350px] h-[350px]" onClick={handle}>
         <ul class="h-full font-bold text-6xl grid grid-cols-2 gap-4">
@@ -96,7 +135,7 @@ function App() {
         </ul>
       </button>
 
-      <div class="flex justify-between h-[60px] w-full max-w-[800px] my-10">
+      <div class="flex justify-between h-[60px] w-full max-w-[800px] mt-10 mb-6">
         <input class="text-2xl shrink-0 rounded p-3 text-black w-full max-w-[500px] bg-slate-300" ref={ref} type="text" onInput={handle2} onPaste={handle2} />
         <div class="flex flex-col items-center">
           <div class="relative basis-1/3 shrink-0">
@@ -119,7 +158,11 @@ function App() {
         </div>
       </div>
 
-      <ol class="AnswerGrid gap-2 w-full max-w-[800px]">
+      <Show when={gameStarted() && !inProgress()}>
+        <GameSummary time={time()} numbersSolved={numbersSolved()} completed={completed()}/>
+      </Show>
+
+      <ol class="AnswerGrid mt-4 gap-2 w-full max-w-[800px]">
         <For each={Object.entries(completed())}>{(item) => 
           {
           const hasSolution = () => {
@@ -132,7 +175,7 @@ function App() {
 
 
           return (
-            <li class={classNames("flex w-full shrink-0 grow-0 rounded justify-center items-center transition-all", {"bg-green-500" : hasSolution() && onTime(), "bg-yellow-500": hasSolution() && !onTime(), "bg-slate-500": !hasSolution()})}>
+            <li class={classNames("flex w-full shrink-0 grow-0 rounded justify-center items-center transition-all", {"bg-green-600" : hasSolution() && onTime(), "bg-yellow-500": hasSolution() && !onTime(), "bg-slate-500": !hasSolution()})}>
               <button onClick={_ => {
                 if (ref) {
                   ref.value = item[1].expr
@@ -147,6 +190,33 @@ function App() {
         </For>
       </ol>
 
+    </div>
+  )
+}
+
+function GameSummary(props: {completed: Record<string, {onTime: boolean}>, numbersSolved: number, time: number}) {
+  const score = () => props.numbersSolved === WINNING_SCORE 
+    ? `${Math.floor((TOTAL_TIME  - props.time) / 60)}:${String((TOTAL_TIME - props.time) % 60).padStart(2, "0")}` 
+    : `${props.numbersSolved}/${WINNING_SCORE}`;
+
+  const scoreSummaryString = () => Object.entries(props.completed).reduce((out, cur, i) => {
+    const space = i % 5 === 4 ? " " : ''
+    return out + (cur[1].onTime ? `🟩${space}` : `⬜${space}`)
+  }, "");
+
+  const clipboardScore = () => props.numbersSolved === WINNING_SCORE
+    ? `${score()}`
+    : `${score()} - ${scoreSummaryString()}`
+
+  return (
+    <div class="flex items-center mb-2 text-xl">
+      <div class="mr-4">
+        Game over!{props.numbersSolved === WINNING_SCORE ? ' You win! ' : ' '}Your score was: <span class="font-bold">{score()}</span>
+      </div>
+      <button onClick={() => {
+        navigator.clipboard.writeText(`${window.location} - ${clipboardScore()}`)
+          .then(() => toast.success("Copied results to clipboard"));
+      }} class="flex items-center bg-green-600 font-semibold rounded py-1 px-3">Share <IoShareSocialSharp class="ml-2" /></button>
     </div>
   )
 }
